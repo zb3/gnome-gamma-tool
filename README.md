948264d67fa9a4f06a4bb739ae28552dde715349
zb3 <onlylogout@gmail.com>
2020-12-18 18:20:30 +0100

    Black metal gods rock n' roll

---
+++ README.md
@@ -0,0 +1,124 @@
+# gnome-gamma-tool
+
+A command-line tool that let's you change gamma, contrast and brightness in GNOME 3 in a persistent manner by creating a color profile with the VCGT table.
+
+## Disclaimer
+**This only works with GNOME**. Tested on Fedora 33 with GNOME 3.38, it might not work with older/newer versions, albeit profiles already generated should continue to work.
+
+This is rather hacky, color profiles were not meant to be used like that. But I think the use case is rare enough to justify this. After all, if your monitor doesn't let you change the gamma, isn't that a problem with the monitor?
+
+See [How it works](#how-it-works) for a more detailed description.
+
+## Usage
+There's no need to install this, just clone this repository:
+```
+git clone https://github.com/zb3/gnome-gamma-tool
+cd gnome-gamma-tool
+```
+
+Then you can invoke the tool like this:
+```
+./gnome-gamma-tool.py
+```
+
+Once the new profile is installed, this tool can be safely  removed.
+
+
+### Adjusting screen gamma
+The `-g` argument let's you specify the gamma correction factor. You can also use the `R:G:B` form to set it for individual channels.
+
+For example:
+```
+./gnome-gamma-tool.py -g 0.8
+```
+or
+```
+./gnome-gamma-tool.py -g 0.8:0.8:0.8
+```
+
+### Adjusting contrast (or inverting colors)
+You can also change the contrast using the `-c` parameter. `1` is the default value, while `-1` will effectively invert the colors. Make sure not to use the `0` value here as it will make your display all grey...
+
+For example:
+```
+./gnome-gamma-tool.py -c -1
+```
+
+Note that it always starts from the initial state, hence the command above will undo any previous gamma adjustments, but you can combine different options:
+
+```
+./gnome-gamma-tool.py -g 0.8 -c 0.5
+```
+
+Per-channel settings work here too, so for instance it's possible to invert the blue channel while keeping red and green intact. Can you guess how this would look like? Give it a try:
+```
+./gnome-gamma-tool.py -c=1:1:-1
+```
+Was that what you expected? :)
+
+### Changing color temperature
+Color temperature can also be changed, use the `-t` option to specify the temperature. Values work just like in `redshift` where `6500` is the neutral value. Note that this doesn't prevent the night light feature from working, it's just that night light adjustments are made "on top of" what gnome-gamma-tool does.
+```
+./gnome-gamma-tool.py -t 5000
+```
+
+### Adjust brightness (kinda)
+You can also adjust brightness with this tool, but it's not possible to increase the physical display brightness, it's only possible to decrease it by decreasing the maximum brightness value. Use the `-b` option to do that, it accepts values from `0` to `1`, where `1` is the neutral value:
+```
+./gnome-gamma-tool.py -b 0.7
+```
+
+There's another option here which let's you *increase* the *minimum* brightness value. It's the `-bm` option which also accepts values from `0` to `1`, this time `0` being the neutral value. After doing:
+```
+./gnome-gamma-tool.py -bm 0.3
+```
+the output value will never be smaller than `0.3` so black won't be black but actually grey, while white will still be white and colors in between will be multiplied accordingly. It's also possible to invert colors using the combination of `-b` and `-bm`:
+```
+./gnome-gamma-tool.py -b 0 -bm 1
+```
+
+And... that's not all, because these also work per individual channels, thanks to the `R:G:B` notation. This opens up new possibilities, like... making your screen more green (which you couldn't achieve with color temperature):
+```
+./gnome-gamma-tool.py -b 1:2:1
+```
+The above gets transformed into `0.5:1:0.5` first, and technically it makes your screen less red and less blue, but practically it makes the screen appear more green. What a useful feature, don't you think so? 
+
+
+
+### If you have multiple displays
+You can specify the display *index* using the `-d` option (the first one has index `0` but that's the default one so you don't need to use this argument in this case...). The order of displays is the same as in the `Settings -> Color` panel. Here comes the example:
+```
+./gnome-gamma-tool.py -d 1 -g 0.7
+```
+
+
+## How it works
+
+Mutter (GNOME's compositor) does not implement any Wayland protocols that could help, so tools like `gammastep` or `wl-gammactl` won't work. Mutter, however, exposes the `SetCrtcGamma` method via D-Bus, and that method really works. Yet it's not how gnome-gamma-tool achieves it's purpose because:
+* the value is not saved anywhere so the effect is not persistent
+* this method is already called by another daemon, so the effect is only temporary.
+
+That method is normally called by the `gsd-color` daemon (indirectly), and currently there are two things that can affect its arguments:
+* VCGT table of the currently enabled color profile (retrieved via colord)
+* color temperature derived from the "night light" settings
+(these two are combined together)
+
+Now, it might seem that the obvious solution here is to patch `gsd-color` to also take other things (like a GSettings property) into account, so you could tweak the gamma via the `gsettings` command. This would also open the possibility of adding a GUI to the Display panel later.
+
+Yet again, this is not how gnome-gamma-tool operates, because patching `gsd-color` only makes sense if this patch is to be merged upstream. Otherwise it's completely unacceptable for me (and probably for you too) to have to apply custom patches and rebuild parts of GNOME. That's why gnome-gamma-tool installs a color profile with the correct "VCGT" table set up. 
+
+`gsd-color` doesn't manage color profiles, it observes them using the API provided by colord. To make a profile recognized by `gsd-color`, these things must be done:
+* color profiles must be enabled for a given device
+* that profile needs to be installed (in the colord database)
+* that profile must be associated with the device
+* that profile must be enabled
+
+All those steps are performed using the API exposed by colord via D-Bus. gnome-gamma-tool doesn't create the new profile from scratch, but it clones the current one and only modifies the VCGT table. By default, the current profile is the one generated by `gsd-color` from EDID data.
+
+`gsd-color` listens for device/profile changes using D-Bus signals so it picks up our profile change and then eventually calls `SetCrtcGamma`. That's it!
+
+What is this "VCGT" then? The VCGT (Video Card Gamma Table) basically maps color channel (R, G and B) values. Like if a channel has value `X` then the VCGT specifies that this should be translated to `Y`. Of course not every input value has a VCGT entry, that table has just 256 entries and the values are interpolated (but this is not done by gnome-gamma-tool).
+
+VCGT makes it possible to change gamma (via exponentiation), contrast and brightness, but it's not possible to change hue this way, because one channel can't affect any other.
+
+
---
+++ gnome-gamma-tool.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python3
+
+import os
+import gi
+import sys
+import tty
+import uuid
+import select
+import termios
+import tempfile
+import argparse
+
+gi.require_version("Colord", "1.0")
+
+from gi.repository import GLib, Colord, Gio
+
+N_SAMPLES = 256
+OUR_PREFIX = "gnome-gamma-tool-"
+
+
+def linear_map(x, smin, smax, dmin, dmax):
+    y = (x - smin) / (smax - smin) * (dmax - dmin) + dmin
+    return y
+
+
+def generate_vcgt(gamma, color_temperature, contrast, bmin, bmax):
+    gamma_factor = [1 / float(ga) for ga in gamma]
+    temp_color = Colord.ColorRGB()
+    vcgt = [None] * N_SAMPLES
+
+    Colord.color_get_blackbody_rgb_full(
+        color_temperature, temp_color, Colord.ColorBlackbodyFlags.USE_PLANCKIAN
+    )
+
+    for i in range(N_SAMPLES):
+        c = Colord.ColorRGB.new()
+
+        # gamma and temperature
+        c.R = temp_color.R * ((i / (N_SAMPLES - 1)) ** gamma_factor[0])
+        c.G = temp_color.G * ((i / (N_SAMPLES - 1)) ** gamma_factor[1])
+        c.B = temp_color.B * ((i / (N_SAMPLES - 1)) ** gamma_factor[2])
+
+        # contrast
+        c.R = max(0, min(contrast[0] * (c.R - 0.5) + 0.5, 1))
+        c.G = max(0, min(contrast[1] * (c.G - 0.5) + 0.5, 1))
+        c.B = max(0, min(contrast[2] * (c.B - 0.5) + 0.5, 1))
+
+        # brightness map
+        c.R = linear_map(c.R, 0, 1, bmin[0], bmax[0])
+        c.G = linear_map(c.G, 0, 1, bmin[1], bmax[1])
+        c.B = linear_map(c.B, 0, 1, bmin[2], bmax[2])
+
+        vcgt[i] = c
+
+    return vcgt
+
+
+def float_per_color(arg, fit=False):
+    if ":" not in arg:
+        arg = f"{arg}:{arg}:{arg}"
+
+    val = list(map(float, arg.split(":")))
+
+    if fit and (maxval := max(val)) > 1:  # the walrus operator says hello
+        val = list(map(lambda x: x / maxval, val))
+
+    return val
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        epilog=f"""for gamma, contrast and brightness levels you can use the form R:G:B to set these values for individual channels.
+
+use the "--arg=value" form if you get the "expected one argument" error
+
+examples:
+{sys.argv[0]} -g 0.8
+{sys.argv[0]} -g 0.8:0.8:0.8""",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    parser.add_argument(
+        "-d", "--display", help="display index (0 is default)", type=int, default=0
+    )
+    parser.add_argument(
+        "-g", "--gamma", help="target gamma correction (1 is neutral)", default="1"
+    )
+    parser.add_argument(
+        "-t",
+        "--temperature",
+        help="target color temperature (6500 is neutral)",
+        type=int,
+        default=6500,
+    )
+    parser.add_argument(
+        "-c",
+        "--contrast",
+        help="target contrast (1 is neutral, -1 to invert)",
+        default="1",
+    )
+    parser.add_argument(
+        "-b",
+        "--brightness",
+        help="maximum output brightness (1 is neutral)",
+        default="1",
+    )
+    parser.add_argument(
+        "-bm",
+        "--min-brightness",
+        help="minimum output brightness, (0 is neutral)",
+        default="0",
+    )
+    parser.add_argument(
+        "-r",
+        "--remove",
+        help="remove the profile created by gnome-gamma-tool",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-y", "--yes", help="don't ask whether new settings are ok", action="store_true"
+    )
+
+    if len(sys.argv) < 2:
+        parser.print_help()
+        sys.exit(2)
+
+    args = parser.parse_args()
+
+    arg_sig_parts = []
+
+    if args.gamma != "1":
+        arg_sig_parts.append(f"g={args.gamma}")
+
+    if args.temperature != 6500:
+        arg_sig_parts.append(f"t={args.temperature}")
+
+    if args.contrast != "1":
+        arg_sig_parts.append(f"c={args.contrast}")
+
+    if args.min_brightness != "0" or args.brightness != "1":
+        arg_sig_parts.append(f"r=[{args.min_brightness},{args.brightness}]")
+
+    args.gamma = float_per_color(args.gamma)
+    args.contrast = float_per_color(args.contrast)
+    args.min_brightness = float_per_color(args.min_brightness, fit=True)
+    args.brightness = float_per_color(args.brightness, fit=True)
+
+    return args, " ".join(arg_sig_parts)
+
+
+def ask_new_settings_ok():
+    fd = sys.stdin.fileno()
+    old_attr = termios.tcgetattr(fd)
+    tty.setcbreak(fd)
+
+    ret = False
+    print("")  # reserve space
+
+    try:
+        seconds_to_wait = 10
+        for s in range(seconds_to_wait):
+            sys.stdout.write("\033[1A\033[2K\r")  # move up and clear the line
+            sys.stdout.write(
+                "Settings will revert in %d seconds...\n" % (seconds_to_wait - s)
+            )
+            sys.stdout.write("Keep these changes? [y/N] ")
+            sys.stdout.flush()
+            i, o, e = select.select([sys.stdin], [], [], 1)
+
+            if i:
+                ret = sys.stdin.read(1) == "y"
+                break
+    except KeyboardInterrupt:
+        pass
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, old_attr)
+
+    print()
+
+    return ret
+
+
+class ProfileMgr:
+    def __init__(self):
+        self.cd = Colord.Client()
+        self.cd.connect_sync()
+        self.devices = self.cd.get_devices_sync()
+
+    def get_device_count(self):
+        return len(self.devices)
+
+    def connect(self, device_idx):
+        self.cdd = self.devices[device_idx]
+        self.cdd.connect_sync()
+
+    def get_current_profile(self):
+        profiles = self.cdd.get_profiles()
+        profile = None
+
+        if profiles:
+            profile = profiles[0]
+            profile.connect_sync()
+
+        return profile
+
+    def remove_profile(self, profile):
+        # colord can handle this properly
+        os.remove(profile.get_filename())
+
+    def clone_profile_data(self, profile):
+        return profile.load_icc(0)
+
+    def new_profile_with_name(self, profile_data, new_name):
+        tmp_path = os.path.join(tempfile.gettempdir(), new_name)
+        profile_data.save_file(
+            Gio.File.new_for_path(tmp_path), Colord.IccSaveFlags.NONE, None
+        )
+
+        try:
+            new_profile = self.cd.import_profile_sync(Gio.File.new_for_path(tmp_path))
+        finally:
+            os.remove(tmp_path)
+
+        self.cdd.add_profile_sync(Colord.DeviceRelation.HARD, new_profile)
+        new_profile.connect_sync()
+
+        return new_profile
+
+    def create_and_set_sRGB_profile(self):
+        profile = self.cd.find_profile_by_filename_sync("sRGB.icc")
+        if not profile:
+            return None
+
+        self.cdd.add_profile_sync(Colord.DeviceRelation.HARD, profile)
+        profile.connect_sync()
+
+        return profile
+
+    def make_profile_default(self, profile):
+        self.cdd.make_profile_default_sync(profile)
+
+    def is_device_enabled(self):
+        return self.cdd.get_enabled()
+
+    def set_device_enabled(self, enabled):
+        self.cdd.set_enabled_sync(enabled)
+
+
+def main():
+    args, arg_signature = parse_args()
+
+    mgr = ProfileMgr()
+
+    display_count = mgr.get_device_count()
+    if args.display >= display_count:
+        exit(f"No such display, found {display_count} displays.")
+
+    mgr.connect(args.display)
+
+    if not mgr.is_device_enabled():
+        # but 'd we have the default edid profile?
+
+        print("Enabling color management for device")
+        mgr.set_device_enabled(True)
+
+    # we get the default one, even if some ggt profiles exist
+    # this is to that you can have more than one ggt profile
+    base_profile = mgr.get_current_profile()
+
+    if not base_profile:
+        print("No default profile, trying sRGB! (this is a bug)")
+        base_profile = mgr.create_and_set_sRGB_profile()
+
+    base_profile_path = base_profile.get_filename()
+    our_profile = OUR_PREFIX in base_profile_path
+    print("Current profile is", base_profile_path)
+
+    if args.remove:
+        if our_profile:
+            print("Removing profile")
+            mgr.remove_profile(base_profile)
+
+    else:
+        profile_data = mgr.clone_profile_data(base_profile)
+
+        title = "gamma-tool: %s" % arg_signature
+        profile_data.set_description("", title)
+        profile_data.set_model("", title)
+
+        unique_id = str(uuid.uuid4())
+        profile_data.add_metadata("uuid", unique_id)  # to make checksum unique
+
+        vcgt = generate_vcgt(
+            args.gamma,
+            args.temperature,
+            args.contrast,
+            args.min_brightness,
+            args.brightness,
+        )
+        profile_data.set_vcgt(vcgt)
+
+        new_profile = mgr.new_profile_with_name(profile_data, OUR_PREFIX + unique_id)
+        print("New profile is", new_profile.get_filename())
+
+        mgr.make_profile_default(new_profile)
+
+        if sys.stdout.isatty() and not args.yes and not ask_new_settings_ok():
+            print("Reverting settings")
+            mgr.make_profile_default(base_profile)
+            mgr.remove_profile(new_profile)
+            mgr.make_profile_default(base_profile)
+
+        elif our_profile:
+            print("Removing old profile")
+            mgr.remove_profile(base_profile)
+
+
+if __name__ == "__main__":
+    main()
